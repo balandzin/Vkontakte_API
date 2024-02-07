e74127ec6ec9d2a6c78ebdcd0a89a76f001af5b1
balandzin <balandzin2602@gmail.com>
2024-02-07 23:40:39 +0300

    Подготовил FriendsViewController

---
+++ Vkontakte_API/FriendCell.swift
@@ -0,0 +1,57 @@
+//
+//  FriendCell.swift
+//  Vkontakte_API
+//
+//  Created by Антон Баландин on 7.02.24.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+    
+    let friendImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 25
+        imageView.backgroundColor = .purple
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(friendImageView)
+        contentView.addSubview(nameLabel)
+        
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            friendImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            friendImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            friendImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            friendImageView.widthAnchor.constraint(equalToConstant: 50),
+            friendImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
--- Vkontakte_API/FriendsViewController.swift
+++ Vkontakte_API/FriendsViewController.swift
@@ -11,19 +11,14 @@ class FriendsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Friends"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        FriendCell()
+    }
 }
